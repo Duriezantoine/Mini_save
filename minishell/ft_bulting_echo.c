6ccb5a94ac72cb1b3b496570399cf016e3c7d581
Antoine Duriez <aduriez@1E7.42angouleme.fr>
2024-10-23 17:56:05 +0200

    normed env

--- minishell/ft_bulting_echo.c
+++ minishell/ft_bulting_echo.c
@@ -6,7 +6,7 @@
 /*   By: aduriez <aduriez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 17:51:35 by aduriez           #+#    #+#             */
-/*   Updated: 2024/10/20 14:53:10 by aduriez          ###   ########.fr       */
+/*   Updated: 2024/10/23 17:49:07 by aduriez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int    bulting_echo(char **cmd, int i)
 {
-    (void)cmd;
     int n;
 
     n = 0;
@@ -25,53 +24,37 @@ int    bulting_echo(char **cmd, int i)
         return(0);
     }
      if (ft_search_n(cmd, i)== 0)
-     {
-        // printf("ici");
         bulting_echo(cmd, i+1);
-     }
     else
+    {
+        if (i != 1)
+            n=1;
+        while(cmd[i])
         {
-            if (i != 1)
-                n=1;
-            while(cmd[i])
-            {
-                write(1, cmd[i], ft_strlen(cmd[i]));
-                if (cmd[i+1]!= NULL)
-                        write(1, " ", 1);
-                i++;
-            }
-            if(n !=1)
-                    write(1, "\n", 1);
-        }   
+            write(1, cmd[i], ft_strlen(cmd[i]));
+            if (cmd[++i] != NULL)
+                write(1, " ", 1);
+        }
+        if(n !=1)
+            write(1, "\n", 1);
+    }   
     return(0);
 }
 
 int ft_search_n(char **cmd, int i)
 {
     int x;
-    x = 0;
-    // int count ;
 
-    // count = 0;
-    // while(cmd[i][x])
-    // {
+    x = 0;
     if (cmd[i][x]== '-' && cmd[i][x+1] == 'n')
     {
         x = x+2;
         if (cmd[i][x]== '\0')
-        {
             return (0);
-        }
     }
     else 
         return(1);
     while(cmd[i][x]== 'n')
         x++;
-    // }
-    if (cmd[i][x]== '\0')
-    {
-        // printf("\nJe suis a la fin de l'atguments\n");
-        return(0);
-    }
-    return(1);
+    return(cmd[i][x] != '\0');
 }
--- minishell/ft_bulting_env.c
+++ minishell/ft_bulting_env.c
@@ -6,7 +6,7 @@
 /*   By: aduriez <aduriez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 18:48:02 by aduriez           #+#    #+#             */
-/*   Updated: 2024/10/23 14:50:02 by aduriez          ###   ########.fr       */
+/*   Updated: 2024/10/23 17:55:26 by aduriez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int bulting_env( t_node *list)
 {
     t_env *tmp;
     t_env *current;
+    t_env *next;
 
     tmp = copy_env(list->env);
     sort_envp(&tmp);
@@ -28,7 +29,7 @@ int bulting_env( t_node *list)
     }
     current = tmp;
     while (current != NULL) {
-        t_env *next = current->next;
+        next = current->next;
         free(current->key);
         free(current->value);
         free(current);
@@ -39,54 +40,56 @@ int bulting_env( t_node *list)
 
 void sort_envp(t_env **env) 
 {
-    t_env *current, *index;
-    char *tmp_key;
-    char *tmp_value;
+    t_env   *current;
+    t_env   *index;
+    char    *tmp_key;
+    char    *tmp_value;
 
-    for (current = *env; current != NULL; current = current->next) 
+    current = *env;
+    while (current)
     {
-        for (index = current->next; index != NULL; index = index->next) 
+        index = current->next;
+        while (index != NULL)
         {
             if (strcmp(current->key, index->key) > 0) 
             {
-                // Échanger les clés
                 tmp_key = current->key;
                 current->key = index->key;
                 index->key = tmp_key;
-
-                // Échanger les valeurs
                 tmp_value = current->value;
                 current->value = index->value;
                 index->value = tmp_value;
             }
+            index = index->next;
         }
+        current = current->next;
     }
 }
 
 t_env *copy_env(t_env *env) 
 {
-    t_env *copy = NULL;
-    t_env *current = env;
-    t_env *prev = NULL;
+    t_env *copy;
+    t_env *current;
+    t_env *prev;
+    t_env *new_node;
 
-    while (current != NULL) {
-        t_env *new_node = (t_env *)malloc(sizeof(t_env));
-        if (new_node == NULL) {
-            perror("malloc");
-            exit(EXIT_FAILURE);
+    current = env;
+    copy = NULL;
+    while (current != NULL)
+    {
+        new_node = (t_env *)ft_calloc(sizeof(t_env), 1);
+        if (new_node != NULL)
+        {
+            new_node->key = strdup(current->key);
+            if (current->value)
+                new_node->value = strdup(current->value);
+            if (copy == NULL) 
+                copy = new_node;
+            else 
+                prev->next = new_node;
+            prev = new_node;
         }
-        new_node->key = strdup(current->key);
-        if (current->value)
-            new_node->value = strdup(current->value);
-        else
-            new_node->value = NULL;
-        new_node->next = NULL;
-        if (copy == NULL) 
-            copy = new_node;
-         else 
-            prev->next = new_node;
-        prev = new_node;
         current = current->next;
     }
-    return copy;
+    return (copy);
 }
