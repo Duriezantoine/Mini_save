6a2b91cd4eff737b01144026ec6fd7abe59e5246
Antoine Duriez <aduriez@1E7.42angouleme.fr>
2024-10-23 18:09:02 +0200

    normed get builtin id

--- minishell/ft_bulting.c
+++ minishell/ft_bulting.c
@@ -6,7 +6,7 @@
 /*   By: aduriez <aduriez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 17:08:04 by dpoltura          #+#    #+#             */
-/*   Updated: 2024/10/20 16:35:40 by aduriez          ###   ########.fr       */
+/*   Updated: 2024/10/23 18:08:46 by aduriez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,42 +17,20 @@ int ft_exceve_bulting(char *str, int len)
 	if (str == NULL)
 		return (0);
 
-    if (strncmp(str, "echo", 5) == 0) {
-      // printf("\n0\n");
+    if (strncmp(str, "echo", 5) == 0)
 		return(0);
-    }
-    if (strncmp(str, "env", 4) == 0) {
-	    // printf("\n1\n");
+    if (strncmp(str, "env", 4) == 0)
        return(0);
-    }
-    if (strncmp(str, "unset", 6) == 0 && len==1) {
-	    // printf("\n3\n");
-
+    if (strncmp(str, "unset", 6) == 0 && len==1)
 		return(0);
-    }    
-    if (strncmp(str, "export", 7) == 0) {
-	    // printf("\n4\n");
-
+    if (strncmp(str, "export", 7) == 0)
        return(0);
-    }
-    if (strncmp(str, "cd", 3) == 0) {
-	    // printf("\n5\n");
-
+    if (strncmp(str, "cd", 3) == 0)
        return(0);
-    }
-    if (strncmp(str, "pwd", 4) == 0) {
-	    // printf("\n6\n");
-
+    if (strncmp(str, "pwd", 4) == 0)
        return(0);
-    }
-    if (strncmp(str, "exit", 5) == 0 ) {
-	    // printf("\n6\n");
-
+    if (strncmp(str, "exit", 5) == 0 )
        return(0);
-    }
-    // if (strncmp(cmd->cmd_and_args[0], "exit",4) == 0) {
-    //     // exit(EXIT_SUCCESS);
-    // }
     return(1);
 }
 
